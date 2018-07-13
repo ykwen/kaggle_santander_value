b22960e94d21da798cfc0c0d9f26c8f7bc2623e3
Yuekun <yg2519@columbia.edu>
2018-07-13 19:18:46 +0800

    Modified

--- main.py
+++ main.py
@@ -1,7 +1,56 @@
 from utils import *
+from sklearn.model_selection import train_test_split
 
 train_file = 'data/train.csv'
 test_file = 'data/test.csv'
+model_check_path = 'model/nn/nn0'
+summary_path = 'model/nn/summary'
 
 train_data = load_csv_data(train_file)
 _, y, x = read_content(train_data)
+
+x = standardize(x)
+#y = normalize(y)
+
+theta = 0.2
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=theta)
+
+batch_size = 1
+nn_size = [512, 128, 16]
+learning_rate = 0.5
+keep_prob = 0.8
+
+
+def train(x, y):
+    with tf.device('/gpu:0'):
+        model = NN(batch_size, len(x[0]), nn_size, learning_rate, summary_path)
+        i = 0
+        while len(x) > 0:
+            xx, x = get_order_batch(x, batch_size)
+            yy, y = get_order_batch(y, batch_size)
+            feed_dict = {model.x: xx, model.y: yy, model.keep_prob: keep_prob}
+            _, summary = model.sess.run([model.train, model.summary], feed_dict=feed_dict)
+            model.writer.add_summary(summary, i * batch_size)
+            i += 1
+        model.saver.save(model.sess, model_check_path)
+
+
+def test(x, y):
+    with tf.device('/gpu:0'):
+        model = NN(batch_size, len(x[0]), nn_size, learning_rate, summary_path)
+        model.saver.restore(model.sess, model_check_path)
+
+        loss = []
+        while len(x) > 0:
+            xx, x = get_order_batch(x, batch_size)
+            yy, y = get_order_batch(y, batch_size)
+            feed_dict = {model.x: xx, model.y: yy, model.keep_prob: 1.0}
+            loss.append(model.sess.run(model.loss, feed_dict=feed_dict))
+    return np.mean(loss)
+
+
+if __name__ == '__main__':
+    score = nn(x_train, x_test, y_train, y_test, [128, 32, 8])
+    #train(x_train, y_train)
+    #score = test(x_train, y_train)
+    print(score)
--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import numpy as np
 import tensorflow as tf
 from sklearn import preprocessing
 from sklearn.decomposition import PCA
+from sklearn.neural_network import MLPRegressor
 
 
 def load_csv_data(path):
@@ -17,7 +18,7 @@ def load_csv_data(path):
 def read_content(data):
     if data[0][1] == 'target':
         ids = [d[0] for d in data[1:]]
-        targets = np.array([d[1] for d in data[1:]]).astype(np.float32)
+        targets = np.array([[d[1]] for d in data[1:]]).astype(np.float32)
         features = np.array([d[2:] for d in data[1:]]).astype(np.float32)
         return ids, targets, features
     else:
@@ -27,7 +28,12 @@ def read_content(data):
 
 
 def cal_loss(preds, y):
-    return np.sqrt(np.average(np.power(np.log(preds + 1) - np.log(y + 1), 2)))
+    r = []
+    if type(y[0]) == 'list':
+        y = [yy[0] for yy in y]
+    for p, yy in zip(preds, y):
+        r.append(np.power(np.log(p + 1) - np.log(yy + 1), 2))
+    return np.sqrt(np.average(r))
 
 
 def write_csv(idx, target, path):
@@ -39,13 +45,21 @@ def write_csv(idx, target, path):
                 f.write('\n')
 
 
+def get_order_batch(data, batch_size):
+    if len(data) >= batch_size:
+        return data[:batch_size], data[batch_size:]
+    else:
+        placeholder = np.zeros(len(data[0]))
+        return data + [placeholder for _ in range(batch_size - len(data))], []
+
+
 # Applying standardize, normalize, PCA, whitening
 def standardize(x):
     return preprocessing.scale(x)
 
 
 def normalize(x, y=None):
-    return preprocessing.Normalizer.fit_transform(x, y)
+    return preprocessing.Normalizer().fit_transform(x, y)
 
 
 def decomposing(x, n, flag):
@@ -53,18 +67,29 @@ def decomposing(x, n, flag):
     return pca.fit_transform(x)
 
 
+def nn(x_train, x_test, y_train, y_test, size):
+    y_train = [y[0] for y in y_train]
+    y_test = [y[0] for y in y_test]
+    l = MLPRegressor(size)
+    l.fit(x_train, y_train)
+    p = l.predict(x_test)
+    return cal_loss(p, y_test)
+
+
 class NN:
-    def __init__(self, batch_size, x_size, size, learning_rate):
+    def __init__(self, batch_size, x_size, size, learning_rate, suumary_path):
         self._lr = learning_rate
+        self.summary_path = suumary_path
 
         self.config = tf.ConfigProto(allow_soft_placement=True)
         self.sess = tf.Session(config=self.config)
 
-        self.x = tf.placeholder([batch_size, x_size], tf.float32)
-        self.y = tf.placeholder([batch_size], tf.float32)
+        self.x = tf.placeholder(tf.float32, [batch_size, x_size])
+        self.y = tf.placeholder(tf.float32, [batch_size, 1])
+        self.keep_prob = tf.placeholder(tf.float32, [1])
         self.w = []
         self.b = []
-        with tf.variable_scope('neuron', reuse=True):
+        with tf.variable_scope('neuron', reuse=tf.AUTO_REUSE):
             for i, s in enumerate(size):
                 if i == 0:
                     self.w.append(tf.get_variable('weight'+str(i), [x_size, s], tf.float32,
@@ -74,21 +99,30 @@ class NN:
                                                   initializer=tf.random_normal_initializer()))
                 self.b.append(tf.get_variable('bias'+str(i), [s], tf.float32,
                                               initializer=tf.constant_initializer(1)))
-            self.w.append(tf.get_variable('output weight', [size[-1], 1], tf.float32,
+            self.w.append(tf.get_variable('output_weight', [size[-1], 1], tf.float32,
                                           initializer=tf.random_normal_initializer()))
-            self.w.append(tf.get_variable('output bias', [1], tf.float32,
+            self.b.append(tf.get_variable('output_bias', [1], tf.float32,
                                           initializer=tf.constant_initializer(1)))
-
-        self.output = tf.Variable('output', [batch_size, x_size], tf.float32)
+        with tf.variable_scope('result', reuse=tf.AUTO_REUSE):
+            self.output = tf.get_variable('output', [batch_size, x_size], tf.float32)
         self.output = self.x
         for one_w, one_b in zip(self.w, self.b):
             self.output = tf.add(tf.matmul(self.output, one_w), one_b)
+
+        self.output = tf.nn.dropout(self.output, keep_prob=self.keep_prob)
 
         self.loss = tf.sqrt(tf.losses.mean_squared_error(self.y, self.output))
 
         self.optimizer = tf.train.AdamOptimizer(self._lr)
 
         self.train = self.optimizer.minimize(self.loss)
+
+        self.sess.run(tf.global_variables_initializer())
+        self.saver = tf.train.Saver()
+
+        tf.summary.scalar('loss', self.loss)
+        self.summary = tf.summary.merge_all()
+        self.writer = tf.summary.FileWriter(self.summary_path)
 
     def set_loss_func(self, new_loss):
         self.loss = new_loss
